65ff0c212926d798b27b69ba7f61e2e3bd56cc5e
LowanWill <lowannwil@byui.edu>
2026-01-10 20:33:09 -0700

    lesson 1 set up

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('John Williams');
+});
+
+app.listen(process.env.PORT || 3000);
+  console.log('Server is running on port ' + (process.env.PORT || 3000));
+
