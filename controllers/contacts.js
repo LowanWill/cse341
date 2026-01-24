51afeaf7bdc0984639badce16b55b9579f77bfb3
LowanWill <lowannwil@byui.edu>
2026-01-24 13:35:58 -0700

    Week 3 personal

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -26,7 +26,62 @@ const getSingle = (req, res, next) => {
     });
 };
 
+const createContact = async (req, res) => {
+    try {
+        if (!req.body || !req.body.firstName) {
+            return res.status(400).json({ message: 'Request body is missing or invalid' });
+        }
+        
+        const contact = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            favoriteColor: req.body.favoriteColor,
+            birthday: req.body.birthday
+        };
+        
+        const response = await mongodb.getDb().collection('contacts').insertOne(contact);
+        if (response.acknowledged) {
+            res.status(201).json(response); 
+        } else {
+            res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+const updateContact = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const contact = {   
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDb().collection('contacts').replaceOne({ _id: userId }, contact);
+    if (response.modifiedCount > 0) {
+        res.status(204).send(); 
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+    }   
+};
+
+const deleteContact = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb.getDb().collection('contacts').deleteOne({ _id: userId });
+    if (response.deletedCount > 0) {
+        res.status(204).send(); 
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+    }
+};
+
 module.exports = {
   getAll,
   getSingle,
+  createContact,
+  updateContact,
+  deleteContact
 };
--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
-  .use('/', require('./routes'));
+// Body parsers MUST come first
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Debug middleware to check if body is being parsed
+app.use((req, res, next) => {
+  console.log('Request body:', req.body);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
@@ -19,6 +27,5 @@ mongodb.initDb((err, mongodb) => {
   } else {
     app.listen(port); 
     console.log(`Connected to DB and listening on port ${port}`);
- 
   }
 });
